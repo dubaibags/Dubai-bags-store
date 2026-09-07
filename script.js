fea42775c23d811cab2fdf876f949d6e8b87f4ec
dubaibags <hesham_elshelh@hotmail.com>
2026-09-07 17:10:08 +0400

    Use Papa.parse to fetch and display product data
    Replaced fetch with Papa.parse to load CSV data and display products.

--- script.js
+++ script.js
@@ -8,12 +8,31 @@ document.getElementById("checkout").addEventListener("click", function () {
         "_blank"
     );
 });
-
 const sheetURL =
 "https://docs.google.com/spreadsheets/d/101ylvyeNgylkRFMf4TnnNncKqwjwpJ6bq9hIMbsDO6g/export?format=csv";
 
-fetch(sheetURL)
-.then(res => res.text())
-.then(data => {
-    console.log(data);
+Papa.parse(sheetURL, {
+    download: true,
+    header: true,
+    complete: function(results) {
+
+     
+        const container = document.getElementById("product-list");
+        container.innerHTML = "";
+
+        results.data.forEach(product => {
+
+            if (product.Available !== "Yes") return;
+
+            container.innerHTML += `
+                <div class="card">
+                    <img src="${product.Image}" alt="${product.Product}" width="180">
+                    <h3>${product.Product}</h3>
+                    <p>AED ${product.Price}</p>
+                    <button>Add to Cart</button>
+                </div>
+            `;
+        });
+
+    }
 });
